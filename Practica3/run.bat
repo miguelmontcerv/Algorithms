edc4f44a43cc8ad82f5d623a7c5fcec7f6c7ee49
miguelmontcerv <63685649+miguelmontcerv@users.noreply.github.com>
2021-05-23 21:03:32 -0500

    practica3

--- Practica3/run.bat
+++
@@ -1,7 +0,0 @@
-:: Forma de invocación para codificar
-::	a.exe ruta_archivo_a_codificar ruta_archivo_codificado ruta_tabla_de_frecuencias
-a.exe "C:\Users\Antonio\Dropbox\CSharpsKit.psd" comprimidos/psd.comprimido salidas/psd.txt 
-:: Forma de invocación para descodificar
-::	a.exe -d ruta_tabla_de_frecuencias ruta_archivo_codificado ruta_de_archvo_descomprimido
-a.exe -d salidas/psd.txt comprimidos/psd.comprimido descomprimidos/x.sql
-::>> salidas/resultados.txt
